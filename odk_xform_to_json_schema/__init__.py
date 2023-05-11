86d1f6ecec00b825f77d41de262d5708037ac95e
Macharia Muguku <machariamuguku@gmail.com>
2023-05-11 17:18:32 +0300

    reword description for clarity, update renamed module

--- odk_xform_to_json_schema/__init__.py
+++ odk_xform_to_json_schema/__init__.py
@@ -1,4 +1,9 @@
-from .xform_to_json_schema import convert_xform_to_json_schema
+"""
+odk_xform_to_json_schema is a Python library
+for generating a JSON Schema from an ODK XForm Schema.
+"""
+
+from .xform_to_json_schema import xform_to_json_schema
 
 __version__ = "1.0.0"
-__all__ = ["convert_xform_to_json_schema"]
+__all__ = ["xform_to_json_schema"]
