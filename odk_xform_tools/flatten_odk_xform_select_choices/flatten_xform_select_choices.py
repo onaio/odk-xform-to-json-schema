b00cc820e35990ce5dd2f2d0ad9d5127fac1e8b7
Macharia Muguku <machariamuguku@gmail.com>
2023-05-23 11:23:29 +0300

    handle external choices - choices in ecternal files, and choices with no labels - e.g for media selects

--- odk_xform_tools/flatten_odk_xform_select_choices/flatten_xform_select_choices.py
+++ odk_xform_tools/flatten_odk_xform_select_choices/flatten_xform_select_choices.py
@@ -3,31 +3,45 @@ from pyxform.aliases import control, select
 
 def flatten_select_dictionary(select_dict):
     field = select_dict["name"]
+    children = select_dict.get("children")
     result = []
 
-    for child in select_dict["children"]:
-        value = child["name"]
-        label = child["label"]
+    if children is None:
+        # external choices
+        if (item_set := select_dict.get("itemset")) is not None:
+            result.append(
+                {
+                    "field": field,
+                    "value": item_set,
+                    "language": "$external",
+                    "label": "$external",
+                }
+            )
+    else:
+        for child in children:
+            value = child["name"]
+            # initialize for media select (no label)
+            label = child.get("label", "undefined")
 
-        if isinstance(label, dict):
-            for language, label_value in label.items():
+            if isinstance(label, dict):
+                for language, label_value in label.items():
+                    result.append(
+                        {
+                            "field": field,
+                            "value": value,
+                            "language": language,
+                            "label": label_value,
+                        }
+                    )
+            else:
                 result.append(
                     {
                         "field": field,
                         "value": value,
-                        "language": language,
-                        "label": label_value,
+                        "language": "undefined",
+                        "label": label,
                     }
                 )
-        else:
-            result.append(
-                {
-                    "field": field,
-                    "value": value,
-                    "language": "undefined",
-                    "label": label,
-                }
-            )
 
     return result
 
